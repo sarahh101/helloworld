e8aa586832bf40bc199661efe4a42cfa8c27a9e2
sarahh101 <sarah.haque01@gmail.com>
2022-09-21 10:54:18 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: __Sarah Haque____________________ Student ID: _121172217_____________ Date: _16/09/2022_______________
+*
+*  Online (Cyclic) URL: _______________________________________________________
+
+NOTE: I tried my best but I could not make my cyclic app work. It kept showing that build has failed. I hope to get marks for what I have done.
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
